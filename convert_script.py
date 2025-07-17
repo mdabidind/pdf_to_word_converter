1a8df73df3e3fbb6dab2fe5177332956967a4c8d
mdabidind <booksofkid@gmail.com>
2025-07-17 17:18:53 +0530

    Update convert_script.py

--- convert_script.py
+++ convert_script.py
@@ -1,19 +1,11 @@
 from pdf2docx import Converter
 import os
 
-input_folder = 'input'
-output_folder = 'output'
+# Make sure input.pdf exists
+input_path = 'input/sample.pdf'
+output_path = 'output/converted_xyz.docx'
 
-os.makedirs(output_folder, exist_ok=True)
-
-for filename in os.listdir(input_folder):
-    if filename.endswith('.pdf'):
-        input_path = os.path.join(input_folder, filename)
-        output_filename = filename.replace('.pdf', '.docx')
-        output_path = os.path.join(output_folder, output_filename)
-
-        cv = Converter(input_path)
-        cv.convert(output_path)
-        cv.close()
-
-print("Conversion completed.")
+# Convert PDF to Word
+cv = Converter(input_path)
+cv.convert(output_path)
+cv.close()
