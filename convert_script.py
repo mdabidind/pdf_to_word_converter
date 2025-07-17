e627933a9251a81e27795dee31ad671882c4ab75
mdabidind <booksofkid@gmail.com>
2025-07-17 17:00:41 +0530

    Update convert_script.py

--- convert_script.py
+++ convert_script.py
@@ -1,11 +1,19 @@
 from pdf2docx import Converter
 import os
 
-input_pdf = 'input.pdf'
-output_docx = 'output/converted_xyz.docx'
+input_folder = 'input'
+output_folder = 'output'
 
-os.makedirs('output', exist_ok=True)
+os.makedirs(output_folder, exist_ok=True)
 
-cv = Converter(input_pdf)
-cv.convert(output_docx, start=0, end=None)
-cv.close()
+for filename in os.listdir(input_folder):
+    if filename.endswith('.pdf'):
+        input_path = os.path.join(input_folder, filename)
+        output_filename = filename.replace('.pdf', '.docx')
+        output_path = os.path.join(output_folder, output_filename)
+
+        cv = Converter(input_path)
+        cv.convert(output_path)
+        cv.close()
+
+print("Conversion completed.")
